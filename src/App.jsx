0155e05ddcdafd4e286a2879619aebdf4711ab23
libertyskies <libertyemerson@outlook.com>
2023-10-24 12:22:32 +0100

    separating comments from article

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import { Routes, Route } from "react-router-dom";
 import SingleArticle from "./components/SingleArticle";
+import AllComments from "./components/AllComments";
 
 function App() {
   return (
@@ -12,6 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route
+          path="/articles/:article_id/comments"
+          element={<AllComments />}
+        />
         <Route path="/articles/:article_id" element={<SingleArticle />} />
         <Route path="*" element={<p>Page not found</p>} />
       </Routes>
--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -3,11 +3,24 @@ import * as api from "../api";
 import VoteAdder from "./VoteAdder";
 
 export default function CommentCard({ comment }) {
+  const options = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+
+  let commentDate = new Date(comment.created_at).toLocaleDateString(
+    undefined,
+    options
+  );
+
   return (
-    <div>
-      <p>{comment.author}</p>
-      <p>{comment.body}</p>
-      <p>{comment.created_at}</p>
+    <div className="comment-container">
+      <p className="comment-author">{comment.author}</p>
+      <p className="comment-body">{comment.body}</p>
+      <p>{commentDate}</p>
       <VoteAdder item={comment} />
     </div>
   );
