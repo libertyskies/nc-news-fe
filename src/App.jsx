3b0db26ae0d2330435dd9e1d636891d00437d167
libertyskies <libertyemerson@outlook.com>
2023-10-25 12:47:51 +0100

    changing article component to allow for topics

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Home from "./components/Home";
 import { Routes, Route } from "react-router-dom";
 import SingleArticle from "./components/SingleArticle";
 import AllComments from "./components/AllComments";
+import ArticleList from "./components/ArticleList";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<ArticleList />} />
+        <Route path="/home" element={<ArticleList />} />
         <Route
           path="/articles/:article_id/comments"
           element={<AllComments />}
         />
         <Route path="/articles/:article_id" element={<SingleArticle />} />
+        <Route path="/:topic" element={<ArticleList />} />
         <Route path="*" element={<p>Page not found</p>} />
       </Routes>
     </>
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
 import * as api from "../api";
+import { useParams } from "react-router-dom";
 import ArticleCard from "./ArticleCard";
 
-export default function Home() {
+export default function ArticleList() {
   const [articles, setArticles] = useState([]);
+  const { topic } = useParams();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    api.getAllArticles().then((articles) => {
-      setArticles(articles);
-      setIsLoading(false);
-    });
-  }, []);
+    if (topic) {
+      api.getArticlesByTopic(topic).then((articles) => {
+        setArticles(articles);
+        setIsLoading(false);
+      });
+    } else {
+      api.getAllArticles().then((articles) => {
+        setArticles(articles);
+        setIsLoading(false);
+      });
+    }
+  }, [topic]);
 
   return (
     <main>
