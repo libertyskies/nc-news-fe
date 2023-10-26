9bd53f0a46c7abce15e15dfba923aba886e2dd6d
libertyskies <libertyemerson@outlook.com>
2023-10-26 11:06:42 +0100

    removing loading state from ArticleCard and using props instead

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -2,32 +2,20 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import * as api from "../api";
 
-export default function ArticleCard({ article_id }) {
-  const [article, setArticle] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    api.getArticleById(article_id).then((article) => {
-      setArticle(article);
-      setIsLoading(false);
-    });
-  }, [article_id]);
-
-  return isLoading ? (
-    <p>Loading article...</p>
-  ) : (
+export default function ArticleCard({ article_id, currentArticle }) {
+  return (
     <Link to={`/articles/${article_id}`}>
       <div className="article-card">
-        <h3 className="article-title"> {article[0].title}</h3>
+        <h3 className="article-title"> {currentArticle.title}</h3>
         <img
           className="article-img"
-          src={article[0].article_img_url}
-          alt={`an image of ${article[0].title}`}
+          src={currentArticle.article_img_url}
+          alt={`an image of ${currentArticle.title}`}
         />
         <div className="article-card-stats">
-          <p className="article-card-votes">Likes: {article[0].votes}</p>
+          <p className="article-card-votes">Likes: {currentArticle.votes}</p>
           <p className="article-card-comment-count">
-            Comments: {article[0].comment_count}
+            Comments: {currentArticle.comment_count}
           </p>
         </div>
       </div>
